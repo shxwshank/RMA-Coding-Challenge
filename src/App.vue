03561dcc239ba70cad2095df58b5850f30c6ed30
Shashank Singh <88741561+shxwshank@users.noreply.github.com>
2022-07-30 06:04:18 +0530

    Code uploaded

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <get-request />
+</template>
+<script>
+import GetRequest from "./GetRequest.vue";
+export default {
+  name: "app",
+  components: {
+    GetRequest
+  }
+}
+</script>
+<style>
+</style>
+
---
+++ src/GetRequest.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="card-text-center-m-3">
+    <ul>
+        <li v-for="item in totalVuePackages" :key="item.name">
+        <div class="Card">
+        <img
+        class="rounded-full-w-full"
+            :src="item.avatar" />
+            <div class="det">
+        <h3 className="itemna">{{item.name}}</h3>
+        <h3 class="itemjob">{{item.job}}</h3>
+        </div>
+        </div>
+        </li>
+    </ul>
+  </div>
+</template>
+<style>
+    
+</style>
+
+<script>
+    export default {
+        name: "get-request",
+        data() {
+            return {
+            totalVuePackages: null
+            };
+        },
+        created() {
+        // Simple GET request using fetch
+        fetch("https://62e3dff33c89b95396d2a865.mockapi.io/users")
+            .then(response => response.json())
+            .then(data => {
+                this.totalVuePackages = data
+            });
+        }
+    }
+</script>
